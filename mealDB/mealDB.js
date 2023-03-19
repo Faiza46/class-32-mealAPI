95af8185eb90a8d1c2dbf1ffe242d01f6b4a9182
Faiza46 <120590382+Faiza46@users.noreply.github.com>
2023-03-19 21:17:14 +0600

    Add files via upload

--- mealDB/mealDB.js
+++ mealDB/mealDB.js
@@ -49,11 +49,11 @@ const displaySearch = (meals) => {
         div.classList.add("col");
         div.innerHTML = ` 
         <div onclick="loadMealDetail(${meal?.idMeal})" class="card">
-                    <img src="${meal?.strMealThumb}" class="card-img-top" alt="...">
+                    <img  src="${meal?.strMealThumb}" class=" img-fluid card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">${meal?.strMeal}</h5>
                         <p class="card-text">${meal?.strInstructions.slice(0, 100)}</p>
-                        <a class="btn btn-primary" target="_blank" href="${meal.strYoutube}">Meal Video</a>
+                        <a class="common-btn btn btn-primary" target="_blank" href="${meal.strYoutube}">Meal Video</a>
                     </div>
                 </div>
         `
